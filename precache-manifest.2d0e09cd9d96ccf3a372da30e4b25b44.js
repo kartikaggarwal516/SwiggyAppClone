89a5b52745227582ed535bf01f02cc6a842cdf00
Kartik <aggarwalkartik96@gmail.com>
2020-02-12 23:14:21 +0530

    Updates

---
+++ precache-manifest.2d0e09cd9d96ccf3a372da30e4b25b44.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afac1d7d3f4d94070f75a56868e1bcb0",
+    "url": "/SwiggyAppClone/index.html"
+  },
+  {
+    "revision": "203b5de2b05218edb278",
+    "url": "/SwiggyAppClone/static/css/main.51096e34.chunk.css"
+  },
+  {
+    "revision": "8d71646b0cdba3f65fa9",
+    "url": "/SwiggyAppClone/static/js/2.65dc7e05.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/SwiggyAppClone/static/js/2.65dc7e05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "203b5de2b05218edb278",
+    "url": "/SwiggyAppClone/static/js/main.fa77f5a7.chunk.js"
+  },
+  {
+    "revision": "d9cfa2800946d4ad93bd",
+    "url": "/SwiggyAppClone/static/js/runtime-main.a0330ecc.js"
+  },
+  {
+    "revision": "510d3e97b1116c440062d60e2b210518",
+    "url": "/SwiggyAppClone/static/media/BhelPuri.510d3e97.jpg"
+  },
+  {
+    "revision": "5602d30291802c04f42679e4033f975d",
+    "url": "/SwiggyAppClone/static/media/Lunchboxheader.5602d302.jpeg"
+  },
+  {
+    "revision": "8fcc84003f97095d600ef3c32bf715a2",
+    "url": "/SwiggyAppClone/static/media/Sticky1.8fcc8400.PNG"
+  },
+  {
+    "revision": "379516e0585482dd0032a3fd13971f7e",
+    "url": "/SwiggyAppClone/static/media/card.379516e0.jpeg"
+  },
+  {
+    "revision": "018deacd42129f130d98ba7edd5a3b32",
+    "url": "/SwiggyAppClone/static/media/home-header1.018deacd.jpeg"
+  },
+  {
+    "revision": "89b00ae97eda740f86fbe8bc159ba8b8",
+    "url": "/SwiggyAppClone/static/media/home-header2.89b00ae9.jpeg"
+  },
+  {
+    "revision": "0ca032052ff68c4512f6b50e69f1a5be",
+    "url": "/SwiggyAppClone/static/media/home-header3.0ca03205.jpeg"
+  },
+  {
+    "revision": "e478811500198f298a0682b3364a5e9e",
+    "url": "/SwiggyAppClone/static/media/home-header4.e4788115.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SwiggyAppClone/static/media/logo.5d5d9eef.svg"
+  }
+]);
